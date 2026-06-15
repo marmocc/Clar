afa8c18b5a6d701eded804e95f3e6b29b1b89a11
marmottatonante <tommaso.detommaso@protonmail.com>
2026-06-15 19:56:27 +0200

    Commented errors

--- clarerr.h
+++ clarerr.h
@@ -1,12 +1,12 @@
 typedef enum ClarErr {
-    SUCCESS = 0,
+    SUCCESS = 0, // No errors or warnings
 
-    _WARNINGS,
-    _END_WARNINGS,
+    _WARNINGS, // Warnings range start
+    _END_WARNINGS, // Warning range end
 
-    _ERRORS,
-    ERROR_INVALID_PARAMETER,
-    ERROR_FAILED_ALLOCATION,
-    ERROR_INVALID_FREE_OPERATION,
-    _END_ERRORS
+    _ERRORS, // Errors range start
+    ERROR_INVALID_PARAMETER, // A parameter that was passed is invalid
+    ERROR_FAILED_ALLOCATION, // An allocation failed to allocate the requested memory
+    ERROR_INVALID_FREE_OPERATION, // A requested free operation was invalid
+    _END_ERRORS // Errors range end
 } ClarErr;
