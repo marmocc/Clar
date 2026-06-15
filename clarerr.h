f6c0655a85ed49571195316c217d3acc348c36f6
marmottatonante <tommaso.detommaso@protonmail.com>
2026-06-15 20:30:30 +0200

    Added invalid state error

--- clarerr.h
+++ clarerr.h
@@ -6,7 +6,7 @@ typedef enum ClarErr {
 
     _ERRORS, // Errors range start
     ERROR_INVALID_PARAMETER, // A parameter that was passed is invalid
+    ERROR_INVALID_STATE, // An object that was passed is in an invalid state
     ERROR_FAILED_ALLOCATION, // An allocation failed to allocate the requested memory
-    ERROR_INVALID_FREE_OPERATION, // A requested free operation was invalid
     _END_ERRORS // Errors range end
 } ClarErr;
