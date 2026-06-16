bbf08ff6404d8d83d7dd0e024943f03bdec2d7b0
marmocc <tommaso.detommaso@protonmail.com>
2026-06-16 11:26:25 +0200

    Renamed ClarErr range delimiters

--- clarerr.h
+++ clarerr.h
@@ -1,12 +1,12 @@
 typedef enum ClarErr {
     SUCCESS = 0, /* No errors or warnings */
 
-    _WARNINGS, /* Warnings range start */
-    _END_WARNINGS, /* Warning range end */
+    RANGE_WARNING_START, /* Warnings range start */
+    RANGE_WARNING_END, /* Warning range end */
 
-    _ERRORS, /* Errors range start */
+    RANGE_ERROR_START, /* Errors range start */
     ERROR_INVALID_PARAMETER, /* A parameter that was passed is invalid */
     ERROR_INVALID_STATE, /* An object that was passed is in an invalid state */
     ERROR_FAILED_ALLOCATION, /* An allocation failed to allocate the requested memory */
-    _END_ERRORS /* Errors range end */
+    RANGE_ERROR_END /* Errors range end */
 } ClarErr;
